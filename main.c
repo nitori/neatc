73a1cde589324ee23e1bf6a6f43a35937b763439
Lars Peter Søndergaard <nitori@chireiden.net>
2017-03-17 14:21:09 +0100

    Test with arguments

--- main.c
+++ main.c
@@ -2,28 +2,27 @@
 #include "neat.h"
 
 
-int main() {
+int main(int argc, char** argv) {
     srand((unsigned int)time(NULL));
 
     Genome *g1 = new_genome(3, 1);
+    mutate_connect(g1);  // add at least one connection
 
-    mutate_connect(g1);
-    mutate_split_connection(g1);
-    mutate_connect(g1);
-    mutate_connect(g1);
-    mutate_connect(g1);
-    mutate_connect(g1);
-    mutate_split_connection(g1);
-    mutate_split_connection(g1);
-    mutate_split_connection(g1);
-    mutate_split_connection(g1);
-    mutate_split_connection(g1);
-    mutate_connect(g1);
-    mutate_connect(g1);
-    mutate_connect(g1);
-    mutate_connect(g1);
-    mutate_connect(g1);
-    mutate_connect(g1);
+    int mutation_count = 10;
+    if (argc > 1) {
+        mutation_count = atoi(argv[1]);
+    }
+
+    int i;
+    int choice;
+    for (i=0; i<mutation_count; i++) {
+        choice = rand() % 2;
+        if (choice == 0) {
+            mutate_connect(g1);
+        } else {
+            mutate_split_connection(g1);
+        }
+    }
 
     save_genome(g1, "genome.txt");
 
