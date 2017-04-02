16c0b2a94b4f18bdad5fa28f653c1aa4900a5c8a
Lars Peter Søndergaard <nitori@chireiden.net>
2017-04-02 16:08:47 +0200

    Add innovation search functions to population

--- include/population.h
+++ include/population.h
@@ -24,6 +24,9 @@ void population_init(Population* population);
 void population_add_species(Population* population, Species* species);
 void population_add_innovation(Population* population, Innovation* innovation);
 
+LinkInnovation* population_find_link_innovation(Population* population, NeuronId node_in_id, NeuronId node_out_id);
+NodeInnovation* population_find_node_innovation(Population* population, INumber inumber);
+
 void population_free(Population* population);
 
 #endif //NEAT_POPULATION_H
--- src/population.c
+++ src/population.c
@@ -7,6 +7,7 @@
 #include "list.h"
 
 #include <stdlib.h>
+#include <innovation.h>
 
 
 Population* new_population() {
@@ -24,6 +25,39 @@ void population_add_species(Population* population, Species* species) {
 
 void population_add_innovation(Population* population, Innovation* innovation) {
     list_append(&population->innovations, new_listitem(innovation));
+}
+
+LinkInnovation* population_find_link_innovation(Population* population, NeuronId node_in_id, NeuronId node_out_id) {
+    int i;
+    ListItem* item;
+    Innovation* innovation;
+    for (i=0; i<population->innovations.size; i++) {
+        item = list_get(&population->innovations, i);
+        innovation = item->data;
+        if (innovation->type == InnovationLinkType) {
+            if (innovation->innovation.link.node_in_id == node_in_id &&
+                    innovation->innovation.link.node_out_id == node_out_id) {
+                return &innovation->innovation.link;
+            }
+        }
+    }
+    return NULL;
+}
+
+NodeInnovation* population_find_node_innovation(Population* population, INumber inumber) {
+    int i;
+    ListItem* item;
+    Innovation* innovation;
+    for (i=0; i<population->innovations.size; i++) {
+        item = list_get(&population->innovations, i);
+        innovation = item->data;
+        if (innovation->type == InnovationNodeType) {
+            if (innovation->innovation.node.inumber == inumber) {
+                return &innovation->innovation.node;
+            }
+        }
+    }
+    return NULL;
 }
 
 void population_free(Population* population) {
